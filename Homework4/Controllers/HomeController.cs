c9adc6f1b0f69770f702abfff49787ec96f5b7a0
William Leingang <wleingang15@mail.wou.edu>
2017-10-27 08:17:21 -0700

    Added basic content to homepage.

---
+++ Homework4/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Page1()
+        {
+            return View();
+        }
+
+        public ActionResult Page2()
+        {
+            return View();
+        }
+
+        public ActionResult Page3()
+        {
+            return View();
+        }
+    }
+}
